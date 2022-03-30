9a1c65c146c533c9484de977ed62a9bacd14ee07
Brad Hansen <bhansen3@uw.edu>
2022-03-29 21:24:14 -0700

    alignment

--- fetch_alightment.sh
+++ fetch_alightment.sh
@@ -1,6 +1,12 @@
+#download SRR files
+
+    # need to download SRR6396795 and SRR6396798
+     #prefetch  SRR6396799 SRR6396800 SRR6396801 SRR6396802
+
 # download FASTqs from SRA
 fasterq-dump --split-files ##addSRR##
 
 ## now we use STAR for alignment and HTseq for counts
 
 ##reference genome 
+ 
