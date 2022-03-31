d878ef87ab040a6e72d028bb417fc1f5bb29cc38
Brad Hansen <bhansen3@uw.edu>
2022-03-31 16:58:31 -0700

    bash file

--- rat_gene_index.sh
+++ rat_gene_index.sh
@@ -8,9 +8,9 @@ cd /bigdata/faustmanlab/bch/rat_assembly
 
 # STAR command
 
-#STAR --runThread 2 \
-#--runMode genomeGenerate \
-#--genomeDir /bigdata/faustmanlab/bch/rat_assembly/ncbi-star_index \
-#--genomeFastaFiles /bigdata/faustmanlab.bch/rat_assembly/Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa \
-#--sjdbGTFfile /bigdata/faustmanlab/bch/rat_assembly/Rattus_norvegicus.mRatBN7.2.105.gtf \
-#--sjdbOverhang 99
+STAR --runThreadN 2 \
+--runMode genomeGenerate \
+--genomeDir /bigdata/faustmanlab/bch/rat_assembly/ncbi-star_index \
+--genomeFastaFiles /bigdata/faustmanlab/bch/rat_assembly/Rattus_norvegicus.mRatBN7.2.dna.toplevel.fa \
+--sjdbGTFfile /bigdata/faustmanlab/bch/rat_assembly/Rattus_norvegicus.mRatBN7.2.105.gtf \
+--sjdbOverhang 99
