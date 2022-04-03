654a61dcf2d5d68396ce47b5d1768390530b0d32
Brad Hansen <bhansen3@uw.edu>
2022-04-03 09:56:53 -0700

    updates

--- rat_gene_fastq.sh
+++ rat_gene_fastq.sh
@@ -1,7 +1,10 @@
+#1/bin/bash
 
-
-# to do fasterq-dump
-fasterq-dump /bigdata/faustmanlab/bch/gse108348/SRR6396793
+export PATH=$PATH:/home/NETID/bhansen3/sratoolkit.3.0.0-ubuntu64/bin 
 
 # to run faster q dump on subfolder and save on bigdata drive
-cat /home/NETID/bhansen3/vscode/testis_dev-geneexp/gse108348_list.txt | xargs fasterq-dump --outdir /bigdata/faustmanlab/bch/gse108348/fastq
+cat /home/NETID/bhansen3/vscode/testis_dev-geneexp/gse85420_list.txt | xargs fasterq-dump --outdir /bigdata/faustmanlab/bch/gse85420/fastq
+
+
+# zip files
+gzip -r /bigdata/faustmanlab/bch/gse85420/fastq
