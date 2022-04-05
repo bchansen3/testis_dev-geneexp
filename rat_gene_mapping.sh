b56cd24aa5ccd0f5696ae9984f3e01aa6df8a7b4
Brad Hansen <bhansen3@uw.edu>
2022-04-05 11:30:47 -0700

    updated readme

--- rat_gene_mapping.sh
+++ rat_gene_mapping.sh
@@ -9,10 +9,10 @@ export PATH=$PATH:/home/NETID/bhansen3/STAR-2.7.10a/source
 
 # define  the gene index
 
-index=/bigdata/faustmanlab/bch/rat_assembly/ncbi-star_index
+index=/bigdata/faustmanlab/bch/rat_assembly_75bp/ncbi-star_index
 
 # provide direction to fastq files
-FILES=/bigdata/faustmanlab/bch/gse53960/fastq/*.fastq.gz
+FILES=/bigdata/faustmanlab/bch/gse85420/fastq/*.fastq.gz
 
 for f in $FILES
 do
@@ -20,7 +20,7 @@ do
     base=$(basename $f .fastq.gz)
     echo $base
     STAR --runThreadN 4 --genomeDir $index --readFilesIn $f --outSAMtype BAM SortedByCoordinate \
-         --quantMode GeneCounts --readFilesCommand zcat --outFileNamePrefix /bigdata/faustmanlab/bch/gse53960/$base"_"
+         --quantMode GeneCounts --readFilesCommand zcat --outFileNamePrefix /bigdata/faustmanlab/bch/gse85420/$base"_"
 done
 
 echo "hopefully that worked!"
